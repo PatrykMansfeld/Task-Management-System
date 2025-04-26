ff9c89b6dcf3fcd559e782266cd8bbf7875886c1
Patryk Artur Mansfeld <patryk.mansfeld@gmail.com>
2025-04-26 20:40:58 +0200

    Basic Structure

---
+++ task-management/src/pages/WelcomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function WelcomePage() {
+  return (
+    <div>
+      <div>WelcomePage</div>
+      <h1>Welcome to task management</h1>
+    </div>
+  );
+}
+
+export default WelcomePage;
